adaa6f4cd77a90921c33cfab432293bc8ceba190
malcolmmacleod <malcolm@macleodhome.com>
2015-07-22 13:09:26 +0100

    Added split view controller and embedded navigation controllers

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,43 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Malcolm Macleod on 22/07/2015.
+//  Copyright (c) 2015 Malcolm Macleod. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+    @IBOutlet weak var graphName: UILabel!
+    
+    var graphTitle : String = "Graph title" {
+        didSet {
+            self.graphName.text = self.graphTitle
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
